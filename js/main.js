e853e18d8262116240583ca7712c48088de9ae26
tart2000 <tart2000design@gmail.com>
2015-05-29 13:25:54 -0400

    snap now working

--- js/main.js
+++ js/main.js
@@ -6,11 +6,12 @@ paper.install(window);
 
 window.onload = function() {
 
+    // Parameters 
+    var snap = 12; 
 
     paper.setup('myCanvas');
 
     var viewSize = view.size;
-    console.log(viewSize);
 
     var raster = new Raster('images/bird.jpg');
     raster.position = new Point(viewSize.width/2,viewSize.height/2);
@@ -24,6 +25,9 @@ window.onload = function() {
         raster.size = new Size(viewSize.width,viewSize.height); // still getting a weird aspect ratio. Needs fixing. 
         
     }
+
+    var trianglesLayer = new Layer();
+    trianglesLayer.moveAbove(raster); 
     
     var tool = new Tool();
     var counter = 0;
@@ -31,52 +35,66 @@ window.onload = function() {
     var point1, point2, point3;
     var avgx, avgy; 
     var snapPosition; 
+    var onSnap = 0; 
 
     var rasterVis = 1;
 
     // Define a mousedown event
     tool.onMouseDown = function(event) {
         // adding circles on click 
-        var circle = new Path.Circle({
-            center: event.point,
-            radius: 4,
-            fillColor: 'white'
-        });
+        makeCircle(event);
 
         // different situations if 1st, 2nd or 3rd point of triangle 
         if (counter == 0) {
             myPath = new Path();
             myPath.strokeColor = 'black';
-            point1 = event.point;
             counter++;
         }
         else if (counter == 1) {
-            point2 = event.point;
             counter++; 
         } else {
-            point3 = event.point;
-            avgx = (point1.x + point2.x + point3.x)/3; 
-            avgy = (point1.y + point2.y + point3.y)/3; 
-            var color = raster.getPixel(avgx, avgy);
+            var color = raster.getPixel(myPath.position);
             myPath.closed = true;
             myPath.fillColor = color;
-            myPath.moveAbove(raster);
-            console.log(color);
+            trianglesLayer.addChild(myPath);
             myPath.strokeWidth = 0;
             counter = 0;
         }
-        myPath.add(event.point);
+        addToTriangle(event);
 
-        circle.onMouseEnter = enter;
-        circle.onMouseLeave = leave; 
     }
 
     function enter (event) {
         this.blendMode = 'normal';
         snapPosition = this.position; 
+        onSnap = 1; 
     }
     function leave (event) {
         this.blendMode = 'multiply';
+        onSnap = 0; 
+    }
+    function makeCircle (event) {
+        if (onSnap == 0) {
+            var circle = new Path.Circle({
+                center: event.point,
+                radius: snap,
+                fillColor: 'white'
+            });
+            console.log('circle created'); 
+            circle.onMouseEnter = enter;
+            circle.onMouseLeave = leave;
+        } else if (onSnap ==1) {
+            // pas besoin d'en refaire un si il y en a déjà un 
+        }
+
+         
+    }
+    function addToTriangle (event) {
+        if (onSnap == 0) {
+            myPath.add(event.point); 
+        } else if (onSnap ==1) {
+            myPath.add(snapPosition);
+        }
     }
 
     // Button to hide and show the background image 
